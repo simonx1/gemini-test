f5e71c38bd039d466805f3e776987e6541c17828
Szymon Kurcab <szymon.kurcab@gmail.com>
2023-12-16 09:19:10 -0500

    Vision test

--- image_test.py
+++ image_test.py
@@ -1,12 +1,9 @@
-# !pip install --upgrade google-cloud-aiplatform
-
-import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Part
 
 def generate():
   model = GenerativeModel("gemini-pro-vision")
   responses = model.generate_content(
-    """"That lawyer is my brother" testified the accountant. But the lawyer testified he didn't have a brother. Who is lying?""",
+    [image1, """What's in that photo? Provide deep analysis."""],
     generation_config={
         "max_output_tokens": 2048,
         "temperature": 0.4,
@@ -18,5 +15,11 @@ def generate():
   print(responses)
 
 
-generate()
+file_path = 'xray.jpeg'
 
+with open(file_path, 'rb') as image_file:
+    file_content = image_file.read()
+
+image1 = Part.from_data(data=file_content, mime_type="image/jpeg")
+
+generate()
