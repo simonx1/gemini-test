25baf7c67a70b2c2ad113022f38744efad672754
Szymon Kurcab <szymon.kurcab@gmail.com>
2023-12-16 10:04:21 -0500

    Script updatre

--- image_test.py
+++ image_test.py
@@ -1,25 +1,30 @@
+import argparse
 from vertexai.preview.generative_models import GenerativeModel, Part
 
-def generate():
-  model = GenerativeModel("gemini-pro-vision")
-  responses = model.generate_content(
-    [image1, """What's in that photo? Provide deep analysis."""],
-    generation_config={
-        "max_output_tokens": 2048,
-        "temperature": 0.4,
-        "top_p": 1,
-        "top_k": 32
-    },
+def generate(file_path):
+    with open(file_path, 'rb') as image_file:
+        file_content = image_file.read()
+
+    image1 = Part.from_data(data=file_content, mime_type="image/jpeg")
+
+    model = GenerativeModel("gemini-pro-vision")
+    responses = model.generate_content(
+        [image1, """What is in this photo? Provide as many details as you can."""],
+        generation_config={
+            "max_output_tokens": 2048,
+            "temperature": 0.4,
+            "top_p": 1,
+            "top_k": 32
+        },
     )
   
-  print(responses)
+    print(responses)
 
+def main():
+    parser = argparse.ArgumentParser(description='Process image file for content generation.')
+    parser.add_argument('file_path', type=str, help='Path to the image file')
+    args = parser.parse_args()
+    generate(args.file_path)
 
-file_path = 'xray.jpeg'
-
-with open(file_path, 'rb') as image_file:
-    file_content = image_file.read()
-
-image1 = Part.from_data(data=file_content, mime_type="image/jpeg")
-
-generate()
+if __name__ == '__main__':
+    main()
